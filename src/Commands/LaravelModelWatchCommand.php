b3fb0aa6e37a0bb37a8bc42a7779ef470278e735
Sami Walbury <hello@mha.systems>
2022-10-19 16:49:46 +0100

    Moved ModelWatching logic to a separate model

--- src/Commands/LaravelModelWatchCommand.php
+++ src/Commands/LaravelModelWatchCommand.php
@@ -2,12 +2,12 @@
 
 namespace Mha\LaravelModelWatch\Commands;
 
-use Doctrine\DBAL\Schema\Column;
 use Illuminate\Database\Console\DatabaseInspectionCommand;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use Mha\LaravelModelWatch\Collections\BaseWatchCollection;
+use Mha\LaravelModelWatch\ModelWatcher;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Output\ConsoleSectionOutput;
 
@@ -19,7 +19,7 @@ class LaravelModelWatchCommand extends DatabaseInspectionCommand
      * @var string
      */
     protected $signature = 'model:watch
-        {model|collection=default : the model class or the name of a collection in your config}
+        {modelOrCollection=default : the model class or the name of a collection in your config}
         {id? : the ID of the model to show. Required if model is specified.}
         {--field=* : specify which field(s) to show}
         {--interval=500 : How often (in milliseconds to poll the database for changes)}
@@ -32,13 +32,7 @@ class LaravelModelWatchCommand extends DatabaseInspectionCommand
      */
     protected $description = 'Watch an Eloquent model for changes';
 
-    protected Collection $fields;
-
-    protected Model $model;
-
     protected int $intervalMilliseconds = 2500;
-
-    protected ConsoleSectionOutput $section;
 
     public function handle(): int
     {
@@ -51,11 +45,16 @@ class LaravelModelWatchCommand extends DatabaseInspectionCommand
         }
 
         // We don't support multiple models.... yet!
-        $this->model = $models->first();
+        $model = $models->first();
+
+        $modelWatcher = ModelWatcher::create(
+            $models->first(),
+            $this->output->getOutput()->section()
+        );
+
 
         while (true) {
-            $this->addVersion($this->model->refresh()->toArray());
-            $this->output();
+            $modelWatcher->refresh();
             usleep(($this->intervalMilliseconds) * 1000);
         }
     }
@@ -72,10 +71,15 @@ class LaravelModelWatchCommand extends DatabaseInspectionCommand
     public function getModels(): Collection
     {
         if ($this->argument('id')) {
-            $class = $this->qualifyModel($this->argument('model'));
+            $class = $this->qualifyModel($this->argument('modelOrCollection'));
 
             /** @var class-string<Model> $class */
-            return collect([$class::findOrFail($this->argument('id'))]);
+            return collect([
+                $class::findOrFail(
+                    $this->argument('id'),
+                    $this->option('field')
+                )
+            ]);
         }
 
         $collectionClass = config('model-watch.collections.default');
@@ -85,108 +89,6 @@ class LaravelModelWatchCommand extends DatabaseInspectionCommand
         ray($collection);
         return $collection->getModels();
 
-    }
-
-    /**
-     * Ask the database for all the columns on the table for the watched model,
-     * and filters them according to --field options if given.
-     */
-    protected function getFields(): Collection
-    {
-        if (! isset($this->fields)) {
-            // Get the list of available fields from the database
-            $schema = $this->model->getConnection()->getDoctrineSchemaManager();
-            $table = $this->model->getConnection()->getTablePrefix().$this->model->getTable();
-            $columns = collect($schema->listTableColumns($table))->map(
-                fn (Column $column) => $column->getName()
-            );
-
-            // If the user has specified some fields, make sure they are all valid.
-            if ($this->option('field')) {
-                $fields = collect($this->option('field'));
-                $invalidFields = $fields->diff($columns);
-                throw_if (
-                    $invalidFields->isNotEmpty(),
-                    'Invalid Field(s): '.$invalidFields->join(', ')
-                );
-                $this->fields = $fields;
-            } else {
-                $this->fields = $columns;
-            }
-            // Make each field a collection to hold the future versions
-            $this->fields = $this->fields->mapWithKeys(fn ($item) => [$item => collect([$item])]);
-        }
-
-        return $this->fields;
-    }
-
-    /**
-     * Given a list of attributes create a new version entry for each field.
-     * Values are all stored as text, and null means the value had not changed.
-     * Any missing attributes are assumed to have not changed.
-     *
-     * @param  array  $attributes
-     * @return void
-     */
-    protected function addVersion(array $attributes): void
-    {
-        $attributes = collect($attributes);
-        $attributes = $this->filterUntrackedAttributes($attributes);
-        $attributes = $this->stringifyAttributes($attributes);
-        $attributes = $this->filterUnchangedAttributes($attributes);
-        if ($attributes->isNotEmpty()) {
-            $this->getFields()->transform(function ($field, $key) use ($attributes) {
-                if ($attributes->has($key)) {
-                    $field[] = $attributes[$key];
-                } else {
-                    $field[] = null;
-                }
-
-                return $field;
-            });
-        }
-    }
-
-    protected function stringifyAttributes(Collection $attributes): Collection
-    {
-        return $attributes->transform(function ($item) {
-            return var_export($item, true);
-        });
-    }
-
-    /**
-     * Given an array of attributes/values remove any which have not changed
-     * since the last version. Also filters out any attributes we are not
-     * monitoring.
-     */
-    protected function filterUnchangedAttributes(Collection $attributes): Collection
-    {
-        $fields = $this->getFields();
-
-        return collect($attributes)->filter(function ($item, $key) use ($fields) {
-            return $fields[$key]->filter()->last() !== $item;
-        });
-    }
-
-    private function filterUntrackedAttributes(Collection $attributes): Collection
-    {
-        return $attributes->intersectByKeys($this->getFields());
-    }
-
-    protected function output()
-    {
-        $headers = $this->getFields()->first()->slice(1)->map(function ($item, $key) {
-            return 'Change '.$key;
-        });
-        $headers->prepend('Field');
-        $table = new Table($this->getSection());
-        $table->setHeaderTitle(class_basename($this->model).' WatchModelCommand.php'.$this->model->getKey());
-        $table->setHeaders($headers->toArray());
-        foreach ($this->fields as $field) {
-            $table->addRow($field->toArray());
-        }
-        $this->getSection()->clear();
-        $table->render();
     }
 
     /**
@@ -217,4 +119,5 @@ class LaravelModelWatchCommand extends DatabaseInspectionCommand
             ? $rootNamespace.'Models\\'.$model
             : $rootNamespace.$model;
     }
+
 }
---
+++ src/ModelWatcher.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Mha\LaravelModelWatch;
+
+
+use Doctrine\DBAL\Schema\Column;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Output\ConsoleSectionOutput;
+
+class ModelWatcher
+{
+    protected Model $model;
+    protected ConsoleSectionOutput $section;
+    protected Collection $fields;
+    protected Collection $watchAttributes;
+
+    public function __construct(Model $model, ConsoleSectionOutput $section)
+    {
+        $this->model = $model;
+        $this->section = $section;
+        $this->watchAttributes = collect($model->getAttributes())->keys();
+    }
+
+    public function watchAttributes(iterable $attributes): self
+    {
+        $this->watchAttributes = collect($attributes);
+        return $this;
+    }
+
+    public function refresh(): self
+    {
+        ray($this->model);
+        ray($this->model->getKey());
+        $this->addVersion($this->model->refresh()->toArray());
+        $this->output();
+        return $this;
+    }
+
+    /**
+     * Ask the database for all the columns on the table for the watched model,
+     * and filters them according to --field options if given.
+     */
+    protected function getFields(): Collection
+    {
+        if (! isset($this->fields)) {
+            // Make each field a collection to hold the future versions
+            $this->fields = $this->watchAttributes->mapWithKeys(fn ($item) => [$item => collect([$item])]);
+        }
+
+        return $this->fields;
+    }
+
+    /**
+     * Given a list of attributes create a new version entry for each field.
+     * Values are all stored as text, and null means the value had not changed.
+     * Any missing attributes are assumed to have not changed.
+     *
+     * @param  array  $attributes
+     * @return void
+     */
+    protected function addVersion(array $attributes): void
+    {
+        $attributes = collect($attributes);
+        $attributes = $this->filterUntrackedAttributes($attributes);
+        $attributes = $this->stringifyAttributes($attributes);
+        $attributes = $this->filterUnchangedAttributes($attributes);
+        if ($attributes->isNotEmpty()) {
+            $this->getFields()->transform(function ($field, $key) use ($attributes) {
+                if ($attributes->has($key)) {
+                    $field[] = $attributes[$key];
+                } else {
+                    $field[] = null;
+                }
+
+                return $field;
+            });
+        }
+    }
+
+    protected function stringifyAttributes(Collection $attributes): Collection
+    {
+        return $attributes->transform(function ($item) {
+            return var_export($item, true);
+        });
+    }
+
+    /**
+     * Given an array of attributes/values remove any which have not changed
+     * since the last version. Also filters out any attributes we are not
+     * monitoring.
+     */
+    protected function filterUnchangedAttributes(Collection $attributes): Collection
+    {
+        $fields = $this->getFields();
+
+        return collect($attributes)->filter(function ($item, $key) use ($fields) {
+            return $fields[$key]->filter()->last() !== $item;
+        });
+    }
+
+    private function filterUntrackedAttributes(Collection $attributes): Collection
+    {
+        return $attributes->intersectByKeys($this->getFields());
+    }
+
+    protected function output()
+    {
+        $headers = $this->getFields()->first()->slice(1)->map(function ($item, $key) {
+            return 'Change '.$key;
+        });
+        $headers->prepend('Field');
+        $table = new Table($this->section);
+        $table->setHeaderTitle(class_basename($this->model).' '.$this->model->getKey());
+        $table->setHeaders($headers->toArray());
+        foreach ($this->fields as $field) {
+            $table->addRow($field->toArray());
+        }
+        $this->section->clear();
+        $table->render();
+    }
+
+    static function create(Model $model, ConsoleSectionOutput $section): self
+    {
+        return new self($model, $section);
+    }
+}
